5eda94a9dc44d0f014784a9185b51d2e8e0c2306
Nishna23 <nishnaegam@gmail.com>
2024-10-29 15:22:29 +0530

    Second

---
+++ MvcCodeFirstApproach_Nishna/Models/EmployeeModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace MvcCodeFirstApproach_Nishna.Models
+{
+    [Table ("tblEmployee")]
+    public class EmployeeModel
+    {
+        [Key]
+        public int Id{ get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeePhone { get; set; }
+        public string Job { get; set; }
+        public double Salary { get; set; }
+
+
+    }
+}
