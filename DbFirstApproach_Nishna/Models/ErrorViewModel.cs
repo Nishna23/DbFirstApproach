66a8b4b009acaded4afc6f17d41393b7f3353b44
Nishna23 <nishnaegam@gmail.com>
2024-10-29 15:13:02 +0530

    First

---
+++ DbFirstApproach_Nishna/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DbFirstApproach_Nishna.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ DbFirstApproach_Nishna/Models/UniversityNishnaContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbFirstApproach_Nishna.Models;
+
+public partial class UniversityNishnaContext : DbContext
+{
+    public UniversityNishnaContext()
+    {
+    }
+
+    public UniversityNishnaContext(DbContextOptions<UniversityNishnaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Course> Courses { get; set; }
+
+    public virtual DbSet<Enrollment> Enrollments { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=NISHNA\\SQLSERVER2022;Database=University_Nishna;User Id=sa;Password=user123;TrustServerCertificate=True;Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(entity =>
+        {
+            entity.HasKey(e => e.CourseId).HasName("PK__Course__C92D7187836DD1F5");
+
+            entity.ToTable("Course");
+
+            entity.Property(e => e.CourseId).HasColumnName("CourseID");
+            entity.Property(e => e.Title).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Enrollment>(entity =>
+        {
+            entity.HasKey(e => e.EnrollmentId).HasName("PK__Enrollme__7F6877FBD5EB2845");
+
+            entity.ToTable("Enrollment");
+
+            entity.Property(e => e.EnrollmentId).HasColumnName("EnrollmentID");
+            entity.Property(e => e.CourseId).HasColumnName("CourseID");
+            entity.Property(e => e.Grade).HasColumnType("decimal(3, 2)");
+            entity.Property(e => e.StudentId).HasColumnName("StudentID");
+
+            entity.HasOne(d => d.Course).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.CourseId)
+                .HasConstraintName("FK_dbo.Enrollment_dbo.Course_CourseID");
+
+            entity.HasOne(d => d.Student).WithMany(p => p.Enrollments)
+                .HasForeignKey(d => d.StudentId)
+                .HasConstraintName("FK_dbo.Enrollment_dbo.Student_StudentID");
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.StudentId).HasName("PK__Student__32C52A79A4DBF302");
+
+            entity.ToTable("Student");
+
+            entity.Property(e => e.StudentId).HasColumnName("StudentID");
+            entity.Property(e => e.EnrollmentDate).HasColumnType("datetime");
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
